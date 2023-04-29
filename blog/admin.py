1090ef0d0e68f75b086a2c4d5137c27ba70ffe0f
heshamprogrammer <haprogrammer6@gmail.com>
2023-04-29 14:18:31 +0200

    'second_commit'

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+admin.site.register(models.Product)
+admin.site.register(models.Img)
+
+
+
+
+
+admin.site.site_header = 'Hesham'
+admin.site.site_title = 'Hesham'
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    firstname = models.CharField( max_length=50)
+    lastname = models.CharField(  max_length=50)
+    
+    def __str__(self):
+        return self.firstname + " " + self.lastname
+    
+    
+class Img(models.Model):
+    imageName = models.CharField(max_length=200)
+    imageDesc = models.TextField()
+    imageTime = models.DateTimeField(auto_now_add=True)
+    imageFile = models.ImageField(upload_to='upload_images/')
+    
+    def __str__(self):
+        return self.imageName
+    
